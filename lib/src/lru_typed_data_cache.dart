aa7cb25dded9cac0bd96536986f12ac9385663b1
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2025-01-04 16:46:16 +0300

    feat!: rename byteCapacity to capacityInBytes
    optimize adding value when it's size is equal to capacityInBytes
    use relative imports

--- lib/src/lru_typed_data_cache.dart
+++ lib/src/lru_typed_data_cache.dart
@@ -1,62 +1,66 @@
 import 'dart:typed_data';
 
-import '../lru.dart';
+import 'lru_cache.dart';
+import 'lru_cache_entry.dart';
 
 
 final class LruTypedDataCache<K, V extends TypedData> extends LruCache<K, V> {
   LruTypedDataCache({
     required int capacity,
-    required this.byteCapacity,
+    required this.capacityInBytes,
   }) : super(capacity);
 
-  /// Space in bytes available for all object.
-  final int byteCapacity;
+  /// Maximum possible total length in bytes for this cache.
+  final int capacityInBytes;
 
-  /// Space in bytes used by all objects in cache.
+  /// Total length in bytes of stored entries.
   int get lengthInBytes => _lengthInBytes;
+
+  /// Total length in bytes of stored entries.
   int _lengthInBytes = 0;
 
   @override
   void touchListEntry(LruCacheEntry<K, V> entry) {
     super.touchListEntry(entry);
 
-    while (_lengthInBytes > byteCapacity)
+    while (_lengthInBytes > capacityInBytes)
       evictListEntry(list.last);
   }
 
   @override
   LruCacheEntry<K, V>? evictListEntry(LruCacheEntry<K, V> entry) {
-    final evicted = super.evictListEntry(entry);
-    if (evicted != null)
-      _lengthInBytes -= evicted.value.lengthInBytes;
-
-    return evicted;
+    final evictedEntry = super.evictListEntry(entry);
+    if (evictedEntry != null)
+      _lengthInBytes -= evictedEntry.value.lengthInBytes;
+    return evictedEntry;
   }
 
   @override
   V? remove(Object? key) {
-    final removed = super.remove(key);
-
-    _lengthInBytes -= removed?.lengthInBytes ?? 0;
-
-    return removed;
+    if (super.remove(key) case final value?) {
+      _lengthInBytes -= value.lengthInBytes;
+      return value;
+    }
   }
 
   @override
   void operator []=(K key, V value) {
-    final lengthInBytes = value.lengthInBytes;
-    if (byteCapacity < lengthInBytes)
-      return;
+    final len = value.lengthInBytes;
+    if (len >= capacityInBytes) {
+      if (len > capacityInBytes)
+        return;
+      // clear is faster than removing all linked list entries individually
+      // because removing single entry you need to relink adjacent entries
+      clear();
+    }
 
-    _lengthInBytes += lengthInBytes;
-
+    _lengthInBytes += len;
     super[key] = value;
   }
 
   @override
   void clear() {
     _lengthInBytes = 0;
-
     super.clear();
   }
 }
