50743c3023210d3bb80781db75a46644c39e98c6
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2025-02-28 00:12:48 +0300

    docs: update example

--- example/example.dart
+++ example/example.dart
@@ -15,28 +15,35 @@ class Key {
 
 void main() {
   {
-    // no weak cache, because String is not supported by Expando
+    // Cannot use weak cache, because String is not supported by Expando
     final cache = LruCache<int, String>(2);
 
     cache[0] = '0';
     cache[1] = '1';
-    cache[2] = '2'; // key 0 is evicted here
 
     cache[0]; // try to touch
 
-    print(cache[0]); // null
-    print(cache[1]); // 1
+    cache[2] = '2'; // key 1 is evicted here
+
+
+    print(cache[0]); // 0
+    print(cache[1]); // null
     print(cache[2]); // 2
   }
   {
-    // use capacityInBytes as additional eviction strategy
+    // Use capacityInBytes as additional eviction strategy, on overflow
+    // old elements will be removed same as normal LRU capacity
     final cache = LruTypedDataCache<int, Uint8List>(capacity: 100, capacityInBytes: 2);
+
     cache[0] = Uint8List(1)..[0] = 0;
     cache[1] = Uint8List(1)..[0] = 1;
+
+    cache[0]; // try to touch
+
     cache[2] = Uint8List(1)..[0] = 2;
 
-    print(cache[0]); // null
-    print(cache[1]); // [1]
+    print(cache[0]); // [0]
+    print(cache[1]); // null
     print(cache[2]); // [2]
   }
   {
@@ -44,14 +51,14 @@ void main() {
 
     cache[0] = Key('0');
     cache[1] = Key('1');
-    cache[2] = Key('2'); // key 0 is evicted from LRU and moved to weak cache
+    cache[2] = Key('2'); // key 0 is evicted from main LRU and moved to weak cache
 
-    // try to touch, if key 0 is not garbage collected yet
+    // if key 0 is not garbage collected yet
     // key 1 is moved to weak cache, and key 0 is restored
     cache[0];
 
-    print(cache[0]); // likely 0
-    print(cache[1]); // likely 1
+    print(cache[0]); // 0 if key 0 wasn't garbage collected
+    print(cache[1]); // 1 if key 0 was garbage collected or if key 1 wasn't GC'd
     print(cache[2]); // 2
   }
 }
