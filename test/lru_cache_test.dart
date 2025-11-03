799a306f2ec585aa7a9deb7279f106e017bef04e
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2025-11-03 17:59:50 +0300

    test: check that list and cache are in sync at all times

--- test/lru_cache_test.dart
+++ test/lru_cache_test.dart
@@ -7,8 +7,24 @@ bool get isDebugMode {
   return isDebug;
 }
 
+class TestObject {
+  TestObject(this.value);
+
+  final String value;
+
+  @override
+  String toString() => value;
+}
+
+void expectLength<K, V extends Object>(LruCache<K, V> cache, dynamic matcher) {
+  expect(cache.length, matcher);
+  expect(cache.cache.length, matcher);
+  expect(cache.list.length, matcher);
+}
+
 void main() {
   group('LruCache', () {
+
     test('should add and retrieve values', () {
       final cache = LruCache<int, String>(2);
       cache[1] = 'one';
@@ -50,21 +66,42 @@ void main() {
     test('should not evict any entries when updating', () {
       final cache = LruCache<int, String>(2);
       cache[1] = 'one';
-      expect(cache.length, equals(1));
       cache[1] = 'uno';
-      expect(cache.length, equals(1));
       cache[2] = 'two';
-      expect(cache.length, equals(2));
       cache[2] = 'dos';
-      expect(cache.length, equals(2));
+
+      expect(cache[1], equals('uno'));
+      expect(cache[2], equals('dos'));
     });
 
-    test('should evict when updating only if the cache is full', () {
+    test('updating same with the value should delay object eviction', () {
+      final cache = LruCache<int, TestObject>(2);
+      final one = TestObject('one');
+      final two = TestObject('two');
+      final three = TestObject('three');
+      cache[1] = one;
+      final oneEntry = cache.cache[1];
+      expect(oneEntry, isNotNull);
+      cache[2] = two;
+      cache[1] = one;
+      cache[3] = three;
+
+      // check that entry is preserved and not recreated
+      expect(cache.cache[1], equals(oneEntry));
+      expect(cache[1], equals(one));
+      expect(cache[2], isNull);
+      expect(cache[3], equals(three));
+    });
+
+    test('should maintain size when updating items', () {
+      // this test checks that list and map is in sync (same size)
+      // during update operation
       const capacity = 3;
       final cache = LruCache<String, int>(capacity);
+      expectLength(cache, equals(0));
       for (var i = 0; i < capacity + 1; i++) {
         cache['key'] = i;
-        expect(cache.length, equals(1));
+        expectLength(cache, equals(1));
       }
     });
 
@@ -101,10 +138,30 @@ void main() {
     });
 
     test('should return correct size', () {
-      final cache = LruCache<int, String>(2);
-      cache[1] = 'one';
+      // this test checks that list and map is in sync (same size)
+      // during multiple add and update operations
+      final cache = LruCache<int, TestObject>(2);
+      final one = TestObject('one');
+      final two = TestObject('two');
+      final three = TestObject('three');
+      final uno = TestObject('uno');
+      final dos = TestObject('dos');
 
-      expect(cache.length, equals(1));
+      expectLength(cache, equals(0));
+      cache[1] = one;
+      expectLength(cache, equals(1));
+      cache[2] = two;
+      expectLength(cache, equals(2));
+      cache[3] = three;
+      expectLength(cache, equals(2));
+      cache[1] = uno;
+      expectLength(cache, equals(2));
+      cache[2] = dos;
+      expectLength(cache, equals(2));
+      // special case: we must ensure that list doesn't grow if we replace
+      // object with the same value
+      cache[2] = dos;
+      expectLength(cache, equals(2));
     });
 
     test('should be able to clear all items', () {
@@ -113,7 +170,7 @@ void main() {
       cache[2] = 'one';
       cache.clear();
 
-      expect(cache.length, equals(0));
+      expectLength(cache, equals(0));
       expect(cache[1], isNull);
       expect(cache[2], isNull);
     });
